f23037e555dad0ad2bb50540f32b2aae87c8ee68
Chacashi <eljohndelapuna@gmail.com>
2025-11-26 11:41:27 -0500

    dasdas

--- src/pages/Game4.tsx
+++ src/pages/Game4.tsx
@@ -1,20 +1,33 @@
 
 
-//import { useEffect } from "react";
+import { useEffect } from "react";
 //import { useNavigate } from "react-router-dom";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function Game4(){
      
 
-       const { unityProvider} = useUnityContext({
+       const { unityProvider, sendMessage} = useUnityContext({
         loaderUrl: "Game4/HouseLand.loader.js",
         dataUrl: "Game4/HouseLand.data.br",
         frameworkUrl: "Game4/HouseLand.framework.js.br",
         codeUrl: "Game4/HouseLand.wasm.br",
     });
 
+    useEffect(() => {
+            return () => {
+                // 1. Silenciar el player de Unity antes de enviarle la instrucción de cierre
+                // Esto silencia la salida de audio del <canvas> de Unity.
+                
     
+                console.log("Saliendo del juego, enviando QuitGame a Unity...");
+                try {
+                    sendMessage("WebCommunication", "DestroyGame");
+                } catch (e) {
+                    console.warn("No se pudo enviar QuitGame (Unity quizá ya cerró)");
+                }
+            };
+        }, [sendMessage]);
 
 
     return(
