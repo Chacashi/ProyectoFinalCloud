dc4020bbe73f5f2b5cf838beb61c9773cfca456b
Chacashi <eljohndelapuna@gmail.com>
2025-12-01 13:54:09 -0500

    dasdasdasd

--- src/pages/Game4.tsx
+++ src/pages/Game4.tsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect,Fragment,useState,useCallback } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function Game4() {
-
-    const { unityProvider, unload } = useUnityContext({
+    const [time, setTime] = useState(""); 
+    const [candy, setCandys] = useState("");
+    const [life, setLife] = useState("");
+    const { unityProvider, unload, sendMessage,addEventListener,removeEventListener } = useUnityContext({
         loaderUrl: "Game4/HouseLand.loader.js",
         dataUrl: "Game4/HouseLand.data.br",
         frameworkUrl: "Game4/HouseLand.framework.js.br",
@@ -11,9 +13,32 @@ function Game4() {
         streamingAssetsUrl: "Game4/StreamingAssets",
     });
 
+     const handleSetTime = useCallback((newTime:any) => {
+        setTime(newTime);
+      }, []);
+    
+      const handleSetCandys = useCallback((newCandy:any) => {
+        setCandys(newCandy);
+      }, []);
+
+      const handleSetLife = useCallback((newLife:any) => {
+        setLife(newLife);
+      }, []);
+
+    useEffect(() => {
+        addEventListener("SetTime", handleSetTime);
+        addEventListener("SetCandys", handleSetCandys);
+        addEventListener("SetLife", handleSetLife);
+
+        return () => {
+        removeEventListener("SetTime", handleSetTime);
+        removeEventListener("SetCandys", handleSetCandys);
+        removeEventListener("SetLife", handleSetLife);
+        };
+    }, [addEventListener, removeEventListener, handleSetTime,handleSetCandys,handleSetLife]);
+
     useEffect(() => {
         return () => {
-  
             unload().catch((e) => {
                 console.warn("Unity no estaba listo para descargarse o ya se cerró:", e);
             });
@@ -22,13 +47,33 @@ function Game4() {
         };
     }, [unload]); 
 
+    function OnPause(){
 
+        sendMessage("UiManager", "OnClickPause")
+    }
+    function OnSettings(){
+
+        sendMessage("UiManager", "OnClickSettings")
+    }
+    function OnDoubt(){
+
+        sendMessage("UiManager", "OnClickDoubt")
+    }
     return (
         <>
             <div className="centered-container">
                 <div className="centered-content">
                     <h1 className="centered-title">Game 4</h1>
+                    <h2>{time}</h2>
+                    <h2>{candy}</h2>
+                    <h2>{life}</h2>
+                     <Fragment>
                     <Unity unityProvider={unityProvider} className="centered-unity" />
+                    <p></p>
+                     <button onClick={OnPause}>Pausa</button>
+                     <button onClick={OnSettings}>Opciones</button>
+                     <button onClick={OnDoubt}>Ayuda</button>
+                     </Fragment>
                 </div>
             </div>
         </>
