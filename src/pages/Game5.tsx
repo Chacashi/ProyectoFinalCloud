d986d5db80aed65cd1472fdaf3dd821e6095ec66
Chacashi <eljohndelapuna@gmail.com>
2025-12-02 02:27:34 -0500

    dsadasd

--- src/pages/Game5.tsx
+++ src/pages/Game5.tsx
@@ -1,14 +1,27 @@
-import { useEffect } from "react";
+import { useEffect,Fragment,useState,useCallback } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 function Game5() {
-
-    const { unityProvider, unload } = useUnityContext({
+    const [time, setTime] = useState("");
+    const { unityProvider, unload,sendMessage,addEventListener,removeEventListener} = useUnityContext({
         loaderUrl: "Game5/MateoSobreRuedas.loader.js",
         dataUrl: "Game5/MateoSobreRuedas.data.br",
         frameworkUrl: "Game5/MateoSobreRuedas.framework.js.br",
         codeUrl: "Game5/MateoSobreRuedas.wasm.br",
     });
+
+    const handleSetTime = useCallback((newTime:any) => {
+        setTime(newTime);
+      }, []);
+
+     useEffect(() => {
+        addEventListener("SetTime", handleSetTime);
+
+        return () => {
+        removeEventListener("SetTime", handleSetTime);
+        };
+    }, [addEventListener, removeEventListener, handleSetTime]);
+
 
     useEffect(() => {
         return () => {
@@ -21,13 +34,20 @@ function Game5() {
         };
     }, [unload]); 
 
-
+    function OnOptions(){
+       sendMessage("UIManager","OptionsPanel") 
+    }
     return (
         <>
             <div className="centered-container">
                 <div className="centered-content">
                     <h1 className="centered-title">Game 5</h1>
+                    <h2>{time}</h2>
+                     <Fragment>
                     <Unity unityProvider={unityProvider} className="centered-unity" />
+                    <p></p>
+                     <button onClick={OnOptions}>Opciones</button>
+                     </Fragment>
                 </div>
             </div>
         </>
